4f036f73a4c994367014e90e7880d69284a83a50
EH2O <edwin.holgerssonolofsson@elev.ga.ntig.se>
2022-11-29 13:52:40 +0100

    Jag har gjort ett Bullet Hell spel

--- src/tag.js
+++ src/tag.js
@@ -1,16 +1,33 @@
 import kaboom from "kaboom";
+
 kaboom({
     width: 1500,
     height: 1000,
     background: [ 255, 125, 0 ]
 });
+
 const hero = "https://media.giphy.com/media/3XUmDYg1EytjNtAu7u/giphy.gif";
 
-const villan = "//media.discordapp.net/attachments/805080222755717160/961534665682997248/SlimeGif.gif";
-const SPEED = 300;
+const villan = "/test/Pog.png";
+const SPEED = 900;
 const ENEMY_SPEED = 250;
+let time = 0;
+let timeA = 0;
+const BULLET_SPEED = 600;
 loadSprite("hero", hero)
-loadSprite("vil", villan)
+loadSprite("vil", villan, {
+    sliceX: 4,
+    width: 112,
+    
+    anims: {
+        "move":{
+            from: 0,
+            to: 3,
+            speed: 5,
+            loop: true,
+        }
+    } 
+})
 
 // Add player game object
 const player = add([
@@ -18,17 +35,21 @@ const player = add([
     pos((100, 100)),
     area(),
     origin("center"),
-    scale(0.25)
-    ])
+    scale(0.1),
+])
+
+
+
 const enemy = add([
     sprite("vil"),
     pos((1000, 1000)),
-    area(),
+    area({ scale: 0.6 }),
     state["move"],
-    origin("center"),
     "enemy",
+    origin("center"),
+    scale(5),
 ])
-
+enemy.play("move")
     onKeyDown("a", () => {
         if(player.pos.x > 0){
            player.move(-SPEED, 0)  
@@ -61,8 +82,47 @@ const enemy = add([
 onUpdate(() => {
     if (!player.exists()) return
 	const dir = player.pos.sub(enemy.pos).unit()
-	enemy.move(dir.scale(ENEMY_SPEED))
+	enemy.move(dir.scale(ENEMY_SPEED))  
+    if(time == 20){
+
+
+		add([
+			pos(enemy.pos),
+			move(dir, BULLET_SPEED),
+			rect(12, 12),
+			area(),
+			cleanup(),
+			origin("center"),
+			color(GREEN),
+			"bullet",
+		])
+        time = 0;
+    }
+    if(timeA == 100){
+        for(let i = 0; i < 360; i += 20 ){
+            add
+            ([
+                pos(enemy.pos),
+                move(Vec2.fromAngle(i), BULLET_SPEED),
+                rect(12, 12),
+                area(),
+                origin("center"),
+                color(GREEN),
+                "bullet",
+            ])
+        }
+        timeA = 0;
+    }
+    timeA += 1;
+   time += 1;
+
 })
+
 player.onCollide("enemy", () =>{
     destroy(player)
 })
+player.onCollide("bullet", () =>{
+    destroy(player)
+})
+
+
